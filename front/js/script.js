47bc8f4c184b22fefe2fec0c92ec570de29ed535
quentingdp <quentin@guillondeprince.fr>
2022-11-14 16:20:56 +0100

    feat: complete javascript for index page

--- front/js/script.js
+++ front/js/script.js
@@ -1,16 +1,46 @@
-const itemHtmlCards = document.getElementById('items');
+//Main function to load when the html loads
+async function main() {
+	const itemList = await getItemList();
+	displayAllItems(itemList);
+}
 
-itemHtmlCards.innerHTML = "<article>Tototo</article>";
+main();
 
+//List all usable functions below.
 //Retrieve item list from the API
-fetch("http://localhost:3000/api/products")
-	.then((res) => {
-		console.log(res.json());
-		return res.json();
-	})
-	.then((value) => {
-		console.log(value);
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+async function getItemList() {
+	const itemList = fetch("http://localhost:3000/api/products")
+		.then((res) => {
+			if (res.ok) {
+				return res.json();
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	return itemList;
+}
+
+//Display all items by looping on the function displayGivenItem that displays one given item
+async function displayAllItems(itemArray) {
+	const itemHtmlCards = document.getElementById('items');
+	for (let i = 0; i < itemArray.length; i++) {
+		displayGivenItem(itemHtmlCards, itemArray[i]);
+	}
+}
+
+//Display a given item by creating the html content of its card, from an item object at API product format
+async function displayGivenItem(itemHtmlCards, item) {
+	var itemHtmlCardContent = "<a href=\"./product.html?id="
+		+ item._id
+		+ "\"><article><img src=\""
+		+ item.imageUrl
+		+ "\" alt=\""
+		+ item.altTxt
+		+ "\"><h3 class=\"productName\">"
+		+ item.name
+		+ "</h3><p class=\"productDescription\">"
+		+ item.description
+		+ "</p></article></a>";
+	itemHtmlCards.innerHTML += itemHtmlCardContent;
+}
